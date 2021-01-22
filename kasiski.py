84e86c38c9e67a1c032e8a72624e8ae49a4f59e5
tiginamaria <tiginamaria@gmail.com>
2021-01-22 10:56:12 +0300

    Add frequency estimation to kasiski

--- kasiski.py
+++ kasiski.py
@@ -27,17 +27,38 @@ class Kasiski:
             key += self._find_key_char(message[k:][::key_len])
         return key
 
-    def _find_key_char(self, message):
-        char_counter = Counter(message)
-        char_counter.update({char: 0 for char in self.alphabet.keys()})
-        message_dict = dict(sorted(char_counter.items(), key=lambda item: item[1]))
-        alphabet_dict = dict(sorted(self.alphabet.items(), key=lambda item: item[1]))
-
+    def _get_most_resent_shift(self, message_dict: dict, alphabet_dict: dict):
+        message_dict = dict(sorted(message_dict, key=lambda item: item[1]))
+        alphabet_dict = dict(sorted(alphabet_dict, key=lambda item: item[1]))
         shifts = []
         for message_char, alphabet_char in zip(message_dict, alphabet_dict):
             shifts.append((ord(message_char) - ord(alphabet_char) + self.alphabet_size) % self.alphabet_size)
         shift_counter = Counter(shifts)
-        shift = shift_counter.most_common(1)[0][0]
+        return shift_counter.most_common(1)[0][0]
+
+    def _get_most_frequency_correlated_shift(self, message_dict: dict, alphabet_dict: dict):
+        message_dict = dict(sorted(message_dict.items()))
+        alphabet_dict = dict(sorted(alphabet_dict.items()))
+
+        best_score = 0
+        best_shift = 0
+        n = len(alphabet_dict)
+        for shift in range(n):
+            dict_keys = list(alphabet_dict.keys())
+            score = sum(abs(message_dict[dict_keys[(i + shift) % n]] ** 2 - alphabet_dict[dict_keys[i]] ** 2)
+                        for i in range(len(dict_keys)))
+            if shift == 0 or score < best_score:
+                best_score = score
+                best_shift = shift
+        return best_shift
+
+    def _find_key_char(self, message):
+        char_counter = Counter(message)
+        char_counter.update({char: 0 for char in self.alphabet.keys()})
+        message_dict = {k: v / len(message) for k, v in char_counter.items()}
+        alphabet_dict = {k: v / 100 for k, v in self.alphabet.items()}
+
+        shift = self._get_most_frequency_correlated_shift(message_dict, alphabet_dict)
         return list(self.alphabet.keys())[shift]
 
     def _get_ngrams_gcds(self, ngrams):
